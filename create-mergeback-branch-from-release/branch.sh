0a84800a60270dbe2bc80195b864cc890eed95d0
Moran Rotman <91007521+mrotman-r7@users.noreply.github.com>
2021-11-17 16:44:35 +0200

    sa

--- create-mergeback-branch-from-release/branch.sh
+++ create-mergeback-branch-from-release/branch.sh
@@ -42,7 +42,7 @@ echo "GIT_USER_NAME: $GIT_USER_NAME"
 echo "GIT_USER_EMAIL: $GIT_USER_EMAIL"
 
 gitConfig
-branchDiff= git diff $VERSION_NAME..main
+branchDiff=(git diff $VERSION_NAME..main)
 if [ -z "${VERSION_NAME}" ]; then
     echo "COMMIT_SHA is empty"
 else if [ ! -z "$branchDiff" ]; then
